422842abd6b091c0016c046b51f814e3c6c68fe5
Sandeep Mansotra <44595297+sandeepmansotra@users.noreply.github.com>
2019-10-30 07:40:14 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://user:user123@cluster0-jt1up.mongodb.net/soicalmedia?retryWrites=true&w=majority",
+    "mongodb+srv://",
   secretOrKey: "secret"
 };
